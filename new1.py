64e2978ccd2116510057e815758ed0a8494f2256
Kartik Motwani <motwanikartk2007@gmail.com>
2026-08-03 00:17:59 +0530

    ye le,daal le multiplication ki batti banake

--- new1.py
+++ new1.py
@@ -5,3 +5,12 @@ print(f"{sum(1,2)}")
 print(f"{sum(3,4)}")
 print(f"{sum(5,6)}")
 print(f"{sum(7,8)}")
+
+def multi(a,b):
+    return a*b
+
+print(f"{multi(1,2)}") 
+print(f"{multi(3,4)}")
+print(f"{multi(5,6)}")
+print(f"{multi(7,8)}")
+
